a5679230bd8987113b56c71ea4384138be7e23ed
Bartlomiej Stepien <masafion@gmail.com>
2021-11-27 16:12:20 +0100

    4.4 Tests

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -1,2 +1,24 @@
-package com.crud.tasks.domain;public class CreatedTrelloCard {
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CreatedTrelloCard {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("shortUrl")
+    private String shortUrl;
+
+//    @JsonProperty("badges")
+//    private Badges badges;
 }
